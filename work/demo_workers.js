b921d831cd79e01fdb031840572f1f09490894b4
Max <54maxwu@gmail.com>
2026-05-15 15:09:28 +0800

    perf(worker): 優化電話號碼 Web Worker 處理效能
    原因：
    1. Regex 每次 onmessage 都重新編譯，造成不必要的 CPU 開銷
    2. globaltel Set 每次訊息都重建，為固定資料應只初始化一次
    3. 排除項目每筆獨立 postMessage，訊息量大時主執行緒 overhead 高
    4. 變數未宣告導致全域汙染，substr 已棄用
    
    調整項目：
    1. demo_workers.js：將 globaltel Set 與 4 個 regex 常數移至 handler 外，僅初始化一次；排除項目批次收集後以 { type: "excluded", data: [] } 一次送出；變數全加 let/const；substr 改 slice
    2. index.htm：新增 case "excluded" 處理批次排除陣列，移除舊 default 字串處理

--- work/demo_workers.js
+++ work/demo_workers.js
@@ -1,71 +1,50 @@
 
+const globaltel = new Set(['0817', '0904', '0905', '0906', '0915', '0916', '0917', '0926', '0927', '0935', '0936', '0937', '0945', '0954', '0955', '0956', '0957', '0958', '0959', '0965', '0966', '0967', '0975', '0976', '0977', '0978', '0979', '0995', '0996', '0997', '09253', '09255', '09256', '09257', '09258'])
+
+const RE_10 = /^9[0-9]{9}/
+const RE_11 = /^09[0-9]{9}/
+const RE_12 = /^639[0-9]{9}/
+const RE_13 = /^6309[0-9]{9}/
+
 onmessage = (event) => {
-    sum = 0
-    sum_del = 0
-    obj = {}
-    newdata = []
-    // globaltel = ['0905', '0906', '0915', '0916', '0917', '0926', '0927', '0935', '0936', '0937', '0945', '0953', '0954', '0955', '0956', '0965', '0966', '0967', '0975', '0977', '0978', '0979', '0995', '0996', '0997']
-    globaltel = new Set(['0817', '0904', '0905', '0906', '0915', '0916', '0917', '0926', '0927', '0935', '0936', '0937', '0945', '0954', '0955', '0956', '0957', '0958', '0959', '0965', '0966', '0967', '0975', '0976', '0977', '0978', '0979', '0995', '0996', '0997', '09253', '09255', '09256', '09257', '09258'])
-    event.data.forEach(d=>{
-        l = d.length
+    let sum = 0
+    let sum_del = 0
+    const newdata = []
+    const excluded = []
+
+    event.data.forEach(d => {
+        const l = d.length
         switch (l) {
             case 10:
-                if(new RegExp('^9[0-9]{9}').test(d)){
-                    sum++
-                    newdata.push("63"+d)
-                }else{
-                    sum_del++
-                    postMessage("排除:"+d)
-                }
-                break;
+                if (RE_10.test(d)) { sum++; newdata.push("63" + d) }
+                else { sum_del++; excluded.push(d) }
+                break
             case 11:
-                if(new RegExp('^09[0-9]{9}').test(d)){
-                    sum++
-                    newdata.push("63"+d.substr(1))
-                }else{
-                    sum_del++
-                    postMessage("排除:"+d)
-                }
-                break;
+                if (RE_11.test(d)) { sum++; newdata.push("63" + d.slice(1)) }
+                else { sum_del++; excluded.push(d) }
+                break
             case 12:
-                if(new RegExp('^639[0-9]{9}').test(d)){
-                    sum++
-                    newdata.push(d)
-                }else{
-                    sum_del++
-                    postMessage("排除:"+d)
-                }
-                break;
+                if (RE_12.test(d)) { sum++; newdata.push(d) }
+                else { sum_del++; excluded.push(d) }
+                break
             case 13:
-                if(new RegExp('^6309[0-9]{9}').test(d)){
-                    sum++
-                    newdata.push("63"+d.substr(3))
-                }else{
-                    sum_del++
-                    postMessage("排除:"+d)
-                }
-                break;
+                if (RE_13.test(d)) { sum++; newdata.push("63" + d.slice(3)) }
+                else { sum_del++; excluded.push(d) }
+                break
             default:
-                sum_del++
-                postMessage("排除:"+d)
-                break;
+                sum_del++; excluded.push(d)
         }
     })
-    newdata.forEach(d=>{
-        tel = "0"+d.substr(2,3)
-        if(globaltel.has(tel)){
-            obj.type = "global"
-        }else{
-            obj.type = "noneglobal"
-        }
-        obj.data = d
-        postMessage(obj)
-    })
-    obj.type = "summary"
-    obj.data = "收錄總數:"+sum+" 排除總數:"+sum_del
-    postMessage(obj)
-    obj.type = "finished"
-    obj.data = ''
-    postMessage(obj)
 
+    if (excluded.length > 0) {
+        postMessage({ type: "excluded", data: excluded })
+    }
+
+    newdata.forEach(d => {
+        const tel = "0" + d.slice(2, 5)
+        postMessage({ type: globaltel.has(tel) ? "global" : "noneglobal", data: d })
+    })
+
+    postMessage({ type: "summary", data: "收錄總數:" + sum + " 排除總數:" + sum_del })
+    postMessage({ type: "finished", data: '' })
 };
